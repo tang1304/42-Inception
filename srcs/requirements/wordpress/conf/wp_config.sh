cdcf2fc9d52d62a58a4ded201d69a5aa7368c0e1
tang1304 <tanguy.gellon@gmail.com>
2024-03-08 15:08:55 +0100

    Trying to run wp container: ERROR : getaddrinfo: Temporary failure in name resolution

--- srcs/requirements/wordpress/conf/wp_config.sh
+++ srcs/requirements/wordpress/conf/wp_config.sh
@@ -1,12 +1,29 @@
-!/bin/bash
+#!/bin/bash
 
 sleep 15
 
-if [! -f /var/www/wordpress/wp-config.php]
+if [ ! -f /var/www/wordpress/wp-config.php ]
 then
-	wp config create --allow root \
-		--dbname = ${DATABASE} \
-		--dbuser = ${DB_USER} \
-		--dbpass = ${DB_PASS} \
-		--dbhost = ${DB_HOST}
+	cd  /var/www/html/wordpress/
+	
+	wp core download --allow-root
+
+	wp config create --allow-root \
+		--dbname=$DATABASE \
+		--dbuser=$DB_USER \
+		--dbpass=$DB_PASSWORD \
+		--dbhost=$DB_HOST
+
+	wp core install --allow-root \
+		--url=$DOMAIN \
+		--title=$WP_TITLE \
+		--admin_user=$WP_ADMIN \
+		--admin_password=$WP_ADMIN_PASS \
+		--admin_email=$WP_ADMIN_MAIL
+
+	wp user create --allow-root \
+		$WP_USER $WP_MAIL --user_password=$WP_USER_PASS
 fi
+
+mkdir -p /run/php
+php-fpm7.4 -F
