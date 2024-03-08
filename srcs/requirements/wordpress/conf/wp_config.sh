34ac9bbc2f10b9e55fcf806414e33543b3fe2c72
tang1304 <tanguy.gellon@gmail.com>
2024-03-08 13:34:05 +0100

    wordpress script done, container builds and run well

--- srcs/requirements/wordpress/conf/wp_config.sh
+++ srcs/requirements/wordpress/conf/wp_config.sh
@@ -4,5 +4,9 @@ sleep 15
 
 if [! -f /var/www/wordpress/wp-config.php]
 then
-	;
+	wp config create --allow root \
+		--dbname = ${DATABASE} \
+		--dbuser = ${DB_USER} \
+		--dbpass = ${DB_PASS} \
+		--dbhost = ${DB_HOST}
 fi
