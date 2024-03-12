ff5d61ab85b5d8d06b6a1dce0e53d027ce9422f6
tang1304 <tanguy.gellon@gmail.com>
2024-03-12 10:17:12 +0100

    Working fine, needs tests on persistence of datas

--- srcs/requirements/wordpress/conf/wp_config.sh
+++ srcs/requirements/wordpress/conf/wp_config.sh
@@ -10,6 +10,7 @@ then
 	wp core download --allow-root
 
 	# Config database
+	echo "Configuring Wordpress database..."
 	wp config create --allow-root \
 		--dbname=$DATABASE \
 		--dbuser=$DB_USER \
@@ -17,6 +18,7 @@ then
 		--dbhost=$DB_HOST
 
 	# Wordpress install with admin infos
+	echo "Installing wordpress..."
 	wp core install --allow-root \
 		--url=$DOMAIN \
 		--title=$WP_TITLE \
@@ -25,6 +27,7 @@ then
 		--admin_email=$WP_ADMIN_MAIL
 
 	# Second user creation
+	echo "Creating second user..."
 	wp user create --allow-root \
 		$WP_USER $WP_MAIL --user_pass=$WP_USER_PASS
 fi
